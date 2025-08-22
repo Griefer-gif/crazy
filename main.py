44afc7bdaa4a675897bd5b214cd07ddca80cf634
Renato de Almeida Diana <82620563+Griefer-gif@users.noreply.github.com>
2025-08-22 20:12:32 -0300

    mano teste sla porra
    porra

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/dadadada")
 async def read_root():
     return {"message": "Hello World"}
 
-@app.get("/teste1")
+@app.get("/testdadadae1")
 async def funcaoteste():
     return {"message": "teste ologoo"}
